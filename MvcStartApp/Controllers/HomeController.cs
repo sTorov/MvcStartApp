a05e92e987e68db69fd2b7c786721526eaf94acc
sTorov <sotorov2011@yandex.ru>
2023-01-05 05:18:48 +0300

    Task 32.11.2

--- MvcStartApp/Controllers/HomeController.cs
+++ MvcStartApp/Controllers/HomeController.cs
@@ -2,22 +2,21 @@
 using MvcStartApp.Models;
 using MvcStartApp.Models.Db;
 using MvcStartApp.Models.Db.Repositories;
-using System.Diagnostics;
 
 namespace MvcStartApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IBlogRepository _repo;
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, IBlogRepository repo)
+        public HomeController(/*ILogger<HomeController> logger,*/ IBlogRepository repo)
         {
-            _logger = logger;
+            //_logger = logger;
             _repo = repo;
         }        
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
---
+++ MvcStartApp/Controllers/LogsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models.Db.Repositories;
+
+namespace MvcStartApp.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly IRequestRepository _repo;
+
+        public LogsController(IRequestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var logs = await _repo.GetAll();
+            return View(logs);
+        }
+    }
+}
